fb20b857ce08728143559199e558bb602cbb2788
Unknown <ticokene22@gmail.com>
2018-07-03 16:55:39 +0700

    rm some fields not need

--- source_code/personal/profile.py
+++ source_code/personal/profile.py
@@ -6,8 +6,6 @@ from .base import TimeStampedModel
 class Profile(TimeStampedModel):
 
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	fb_id = models.CharField(max_length=50, blank=True, null=True, unique=True)
-    fb_access_token = models.CharField(max_length=200, blank=True, null=True, unique=True)
     email = models.EmailField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=13, blank=True, null=True)
 
